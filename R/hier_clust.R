94cc610fabbfbb64dd97f2509724423789ebab4c
jhavstad8 <jhavstad@calpoly.edu>
2022-05-23 20:13:58 -0700

    wrote functions first attempt

---
+++ R/hier_clust.R
@@ -0,0 +1,34 @@
+#' Implements agglomerative hierarchical clustering
+#'
+#' @param dat A data frame
+#' @param k The number of clusters you want
+#' @return A data frame with the cluster assignments
+#'
+#' @import dplyr
+#' @export
+
+hier_clust <- function(dat,k){
+    dat[,3] <- 1
+    for(clust in 1:k){
+        r1 <- c()
+        r2 <- c()
+        distance <- c()
+        for(row in 1:nrow(dat)){
+            set_x <- dat[row,1]
+            set_y <- dat[row,2]
+            for(obs in (row+1):nrow(dat)){
+                dist <- sqrt((set_x - dat[obs,1])^2 + (set_y - dat[obs,2])^2)
+                r1 <- c(r1, row)
+                r2 <- c(r2, obs)
+                distance <- c(distance,dist)
+            }
+        }
+        min_d <- which.min(distance)
+        find_r1 <- r1[min_d]
+        find_r2 <- r2[min_d]
+        meanx <- mean(c(dat[find_r1,1], dat[find_r2,1]))
+        meany <- mean(c(dat[find_r1,2], dat[find_r2,2]))
+ # find way to replace rows with new obs (meanx,meany)
+}
+}
+
---
+++ R/k_means.R
@@ -0,0 +1,52 @@
+#' Implements a basic k-means algorithm
+#'
+#' @param dat A data frame
+#' @param k The number of observations to use as starting points
+#' @param pca An option if you want to perform PCA before doing the k_means clustering
+#'
+#' @return A list with the cluster assignments and total sum of squares
+#'
+#' @import dplyr
+#' @export
+
+
+
+k_means <- function(dat,k,pca=FALSE){
+    pts <- dat[sample(nrow(dat),k),]
+    x <- dat[,1]
+    y <- dat[,2]
+    dist <- data.frame(matrix(ncol=k,nrow=length(x)))
+    cluster <- c()
+    for(i in 1:k){
+        for(i in 1:length(x)){
+        dist[i,k] <- sqrt((pts[k,1] - x[i])^2 + (pts[k,2] - y[i])^2)
+        }
+    }
+    for(row in 1:nrow(dist)){
+        shortest <- which.min(dist[i,])
+        cluster <- c(cluster,shortest)
+    }
+    dat <- cbind(dat,cluster)
+    mean_x <- tapply(dat[,1], dat[,3],mean)
+    mean_y <- tapply(dat[,2], dat[,3], mean)
+
+    # repeat steps
+    for(step in 1:100){
+    for(col in 1:k){
+        for(i in 1:length(x)){
+            dist[i,k] <- sqrt((mean_x[k] - x[i])^2 + (mean_y[k] - y[i])^2)
+        }
+    }
+    clust <- c()
+    for(row in 1:nrow(dist)){
+        shortest <- which.min(dist[row,])
+        clust <- c(clust,shortest)
+    }
+    dat[,3] <- clust
+    mean_x <- tapply(dat[,1], dat[,3],mean)
+    mean_y <- tapply(dat[,2], dat[,3], mean)
+    }
+    results <- cluster
+    return(results)
+}
+

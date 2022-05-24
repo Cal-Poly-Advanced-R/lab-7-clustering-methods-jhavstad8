7d9e6f1ef61b6158acce94639fe0c7a2394eb930
jhavstad8 <jhavstad@calpoly.edu>
2022-05-24 10:29:13 -0700

    update k_means

--- R/k_means.R
+++ R/k_means.R
@@ -13,37 +13,48 @@
 
 
 k_means <- function(dat,k,pca=FALSE){
+    # sample observations from data set to be starting points
     pts <- dat[sample(nrow(dat),k),]
+
+    # turn columns in dataset into x and y vectors
     x <- dat[,1]
     y <- dat[,2]
+
+    # create empty data frame for our distances
     dist <- data.frame(matrix(ncol=k,nrow=length(x)))
+
+    # find distances between each point in our data set and each sample observation
     cluster <- c()
-    for(i in 1:k){
+    for(clust in 1:k){
         for(i in 1:length(x)){
-        dist[i,k] <- sqrt((pts[k,1] - x[i])^2 + (pts[k,2] - y[i])^2)
+        dist[i,clust] <- sqrt((pts[clust,1] - x[i])^2 + (pts[clust,2] - y[i])^2)
         }
     }
+    # find which starting point each observation is closest to
     for(row in 1:nrow(dist)){
         shortest <- which.min(dist[i,])
         cluster <- c(cluster,shortest)
     }
+    # bind cluster vector to dataset, showing which cluster each observation is in
     dat <- cbind(dat,cluster)
+
+    # find mean x and y for each cluster, those are new starting points
     mean_x <- tapply(dat[,1], dat[,3],mean)
     mean_y <- tapply(dat[,2], dat[,3], mean)
 
     # repeat steps
-    for(step in 1:100){
-    for(col in 1:k){
+    for(step in 1:50){
+    for(clust in 1:k){
         for(i in 1:length(x)){
-            dist[i,k] <- sqrt((mean_x[k] - x[i])^2 + (mean_y[k] - y[i])^2)
+            dist[i,clust] <- sqrt((mean_x[clust] - x[i])^2 + (mean_y[clust] - y[i])^2)
         }
     }
-    clust <- c()
+    cluster <- c()
     for(row in 1:nrow(dist)){
         shortest <- which.min(dist[row,])
-        clust <- c(clust,shortest)
+        cluster <- c(clusteer,shortest)
     }
-    dat[,3] <- clust
+    dat[,3] <- cluster
     mean_x <- tapply(dat[,1], dat[,3],mean)
     mean_y <- tapply(dat[,2], dat[,3], mean)
     }
